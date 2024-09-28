5450ffac8d1aad580c95c9cd9f7ed66a2ac0189a
Anne Muriuki <muriukianne05@gmail.com>
2024-09-28 13:12:33 +0300

    add grade system

---
+++ generator.js
@@ -0,0 +1,47 @@
+// // GRADE SYSTEM
+// Used let to declare the variable name
+// Passed the value 90 as a number
+let mark =90
+
+// use conditional statements if, else if, and else in order to allow the program to make a descision from the value.
+// used console.log to print the  output when run
+
+if(mark >0 && mark <40)
+    {
+     console.log("Grade E")
+    }
+
+else if(mark >=40 && mark <49)
+   {
+     console.log("Grade D")
+   }
+
+else if(mark  >=49 && mark<=59)
+   {
+     console.log("Grade C")
+   }
+
+else if(mark >=60 && mark <79)
+    {
+     console.log("Grade B")
+    }
+
+else if(mark >=79  && mark <=100)
+    {
+     console.log("Grade A")
+    }
+
+else {console.log("Invalid Input")}
+
+// The grade will be seen in the output as "Grade A" since the value is betwwen 79 and 90
+
+
+
+
+
+
+
+
+
+
+
